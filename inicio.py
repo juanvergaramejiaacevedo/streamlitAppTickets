1203d314fcd44b6b23fb99f450fa396bc9747800
juanvergaramejiaacevedo <juan.vergara@mejiaacevedo.com.co>
2025-04-24 14:04:11 -0500

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import login as login
 
-archivo = __file__.split("/")[-1]
+archivo = __file__.split("\\")[-1]
 login.generarLogin(archivo)
+
 if 'correo_electronico' in st.session_state:
     st.header('Página :orange[principal]')
     #st.subheader('Información página principal')

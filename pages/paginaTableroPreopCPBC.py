b4045eeaf983f97b90de1bfd2c0d6db1c735027f
Juan Daniel Vergara Nieto <juan.vergara@mejiaacevedo.local>
2026-01-29 09:16:13 -0500

    Se realizan cambios en los enlaces a los dashboards de los preoperacionales

--- pages/paginaTableroPreopCPBC.py
+++ pages/paginaTableroPreopCPBC.py
@@ -8,8 +8,8 @@ login.generarLogin(archivo)
 
 if 'correo_electronico' in st.session_state:
     
-    st.subheader('Información | :orange[Tablero de Power BI - SGI-GH Q+]')
+    st.subheader('Información | :orange[Tablero de Power BI - App Preoperacionales Control Pérdidas Bolívar Centro e ISVIMED]')
     
     components.html("""
-        <iframe title="dashboard_CPBC_ISV_VF" width="1750" height="1060" src="https://app.powerbi.com/reportEmbed?reportId=00e8a58e-7d9b-4d7e-a19e-4c91264ce7b8&autoAuth=true&ctid=fa1e9b00-008a-412d-bc71-ae528086a852" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="dashboard_CPBC_ISV_VF" width="1750" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiZDBlMzA3ZWYtODQyNy00MGE3LTg0NzUtMzU1NmFlNmU0MWRkIiwidCI6ImZhMWU5YjAwLTAwOGEtNDEyZC1iYzcxLWFlNTI4MDg2YTg1MiJ9&pageName=652e28b69898452bab3e" frameborder="0" allowFullScreen="true"></iframe>
     """, height=1060)

7d58efb8e6d3fb6b09a3ab090ec3c6fbda9a7b52
Juan Daniel Vergara Nieto <juan.vergara@mejiaacevedo.local>
2026-05-08 15:48:38 -0500

    Se realiza el cambio del nombre del tìtulo del Dashboard de Preoperacionales de CPBC/ISV

--- pages/paginaTableroPreopCPBC.py
+++ pages/paginaTableroPreopCPBC.py
@@ -11,5 +11,5 @@ if 'correo_electronico' in st.session_state:
     st.subheader('Información | :orange[Tablero de Power BI - App Preoperacionales Control Pérdidas Bolívar Centro e ISVIMED]')
     
     components.html("""
-        <iframe title="dashboard_CPBC_ISV_VF" width="1750" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYzM3MmM3YzctZTYzNi00M2JmLWI3NWEtNTAzN2VkNjIwNzlmIiwidCI6ImZhMWU5YjAwLTAwOGEtNDEyZC1iYzcxLWFlNTI4MDg2YTg1MiJ9" frameborder="0" allowFullScreen="true"></iframe>
+        <iframe title="dashboard_CPBC_ISV_VF_BDL" width="1750" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiYzM3MmM3YzctZTYzNi00M2JmLWI3NWEtNTAzN2VkNjIwNzlmIiwidCI6ImZhMWU5YjAwLTAwOGEtNDEyZC1iYzcxLWFlNTI4MDg2YTg1MiJ9" frameborder="0" allowFullScreen="true"></iframe>
     """, height=1060)
